dca290bf90fdb6d680070c063f152235b0479e9e
iruedar <nachete.rueda@gmail.com>
2022-12-17 00:30:19 +0100

    Add username no repeated

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -4,6 +4,7 @@ package controllers;
 import helpers.HashUtils;
 import models.User;
 import play.mvc.Controller;
+import play.i18n.Messages;
 
 public class PublicContentBase extends Controller {
 
@@ -12,9 +13,14 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        if (User.loadUser(username) == null){
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        }else{
+            flash.put("repeated", Messages.get("Public.login.userRepeated"));
+            register();
+        }
     }
 
     public static void registerComplete(){
